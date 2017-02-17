29f4113a2c38d94a1ffe6f7f4d1e51a8a9da6804
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-02-17 16:25:21 +0100

    tidy up

--- tools/explore-lektor.py
+++
@@ -1,9 +0,0 @@
-from lektor.project import Project
-
-project = Project.discover()
-env = project.make_env()
-pad = env.new_pad()
-root_record = pad.root
-print(root_record)
-for c in root_record.children:
-    print(c)
---
+++ utils/explore-lektor.py
@@ -0,0 +1,23 @@
+""" for exploring lektor internally """
+from lektor.db import Query
+from lektor.project import Project
+
+
+def queries(pad):
+    q = Query('/articles', pad).order_by('-lastModified')
+    for e in q:
+        print(e)
+
+
+def children(pad):
+    root_record = pad.root
+    print(root_record)
+    for c in root_record.children:
+        print(c)
+
+
+if __name__ == '__main__':
+    _project = Project.discover()
+    _env = _project.make_env()
+    _pad = _env.new_pad()
+    queries(_pad)
