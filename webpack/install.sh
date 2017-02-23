1627c674662e1ab021f8fef57e3ac9645685151a
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-02-23 22:41:14 +0100

    move stuff around, make build work

---
+++ webpack/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+npm install --save-dev \
+    webpack@1.14.0 \
+    extract-text-webpack-plugin@1.0.1 \
+    babel-loader \
+    css-loader \
+    file-loader \
+    node-sass \
+    sass-loader \
+    style-loader \
+    url-loader \
+    webpack babel-core \
--- webpack/webpack.config-2-WIP.js
+++ webpack/webpack.config-2-WIP.js
@@ -1,24 +1,47 @@
+// This is only partially adapted for webpack 2
+// ExtractTextPlugin is still playing up
+// better wait till 2 release has settled
+
 var webpack = require('webpack');
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+// var loaders = [
+//   {
+//     loader: 'css-loader',
+//     options: {
+//       modules: true
+//     }
+//   },
+//   {
+//     loader: 'postcss-loader'
+//   },
+//   {
+//     loader: 'sass-loader'
+//   }
+// ]
+
+ExtractTextPlugin.extract({
+  fallbackLoader: 'style-loader',
+  loader: loaders,
+})
 
 var options = {
   entry: {
     'app': './js/main.js',
-    'styles': './sass/style.sass'
+    'styles': './scss/main.scss'
   },
   output: {
-    path: path.dirname(__dirname) + '/assets/static/packed',
+    path: path.dirname(__dirname) + '/assets/static/gen',
     filename: '[name].js'
   },
   devtool: '#cheap-module-source-map',
   resolve: {
-    modulesDirectories: ['node_modules'],
+    modules: ['node_modules'],
     extensions: ['', '.js']
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -26,10 +49,6 @@ var options = {
       },
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
-      },
-      {
-        test: /\.sass$/,
         loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
       },
       {
@@ -50,5 +69,7 @@ var options = {
     new webpack.optimize.DedupePlugin()
   ]
 };
+
+
 
 module.exports = options;
