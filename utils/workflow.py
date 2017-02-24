0f5bf75d425b15bd62d52d6f1c3da168b9bc7cfd
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-02-24 20:26:08 +0100

    major rework of styles and workflow

--- utils/workflow.py
+++ utils/workflow.py
@@ -1,11 +1,11 @@
 """
 Helpers for my evolving workflow.
 
-    draft art "My super article"
+    draft [art] "My super article"
 
 creates a prepared md file with all the necessary settings to work on.
 
-    publish drafts/art-my-super-article.md
+    publish drafts/my-super-article.md
 
 will make the necessary adjustments and publish it in the contents.
 
@@ -23,47 +23,38 @@ from string import Template
 
 from lektor.utils import slugify
 
-_HERE = os.path.dirname(__file__)
-BLUEPRINTS_PATH = os.path.join(_HERE, 'blueprints')
-PROJECT_PATH = os.path.join(_HERE, '..')
+HERE = os.path.dirname(__file__)
+PROJECT_PATH = os.path.join(HERE, '..')
 DRAFTS_PATH = os.path.join(PROJECT_PATH, 'drafts')
 CONTENT_PATH = os.path.join(PROJECT_PATH, 'content')
 
-ARTICLE = 'art'
-DIARY = 'dia'
-KINDS = [ARTICLE, DIARY]
-KIND_DST_MAP = {
-    ARTICLE: os.path.join(CONTENT_PATH, 'articles'),
-    DIARY: os.path.join(CONTENT_PATH, 'diary'),
-}
-
 
 def draft():
-    kind, title = sys.argv[1:3]
-    assert kind in KINDS, kind
-    with open(os.path.join(BLUEPRINTS_PATH, '%s.md' % kind)) as f:
+    title = sys.argv[1]
+    with open(os.path.join(HERE, 'article-blueprint.md')) as f:
         content = f.read()
     rep = dict(title=title)
     content = Template(content).safe_substitute(rep)
-    dst = os.path.join(DRAFTS_PATH, '%s-%s.md' % (kind, slugify(title)))
+    dst = os.path.join(DRAFTS_PATH, '%s.md' % slugify(title))
     assert not os.path.exists(dst), dst
     with open(dst, 'w') as f:
         f.write(content)
 
 
 def publish():
-    path = sys.argv[1]
-    with open(os.path.join(PROJECT_PATH, path)) as f:
+    srcPath = sys.argv[1]
+    with open(srcPath) as f:
         content = f.read()
     rep = dict(date=datetime.now().strftime('%Y-%m-%d'))
     content = Template(content).safe_substitute(rep)
-    name = os.path.splitext(os.path.basename(path))[0]
-    kind, name = name.split('-', 1)
-    dst = os.path.join(KIND_DST_MAP[kind], name, 'contents.lr')
-    os.mkdir(os.path.dirname(dst))
+    slug = os.path.splitext(os.path.basename(srcPath))[0]
+    containerPath = os.path.join(CONTENT_PATH, slug)
+    assert not os.path.exists(containerPath), containerPath
+    os.mkdir(containerPath)
+    dst = os.path.join(containerPath, 'contents.lr')
     with open(dst, 'w') as f:
         f.write(content)
-    os.remove(path)
+    os.remove(srcPath)
 
 
 def deploy():
