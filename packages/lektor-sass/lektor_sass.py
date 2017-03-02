26791bee19d4d6b1e36d0e514227dcc22371d004
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-03-02 22:09:29 +0100

    tidy up

--- packages/lektor-sass/lektor_sass.py
+++ packages/lektor-sass/lektor_sass.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
 import os
 import subprocess
 
 from lektor.pluginsystem import Plugin
 from lektor.reporter import reporter
-from lektor.utils import portable_popen
 
 
 class SassPlugin(Plugin):
@@ -13,46 +11,41 @@ class SassPlugin(Plugin):
     rubyPathCmd = ["ruby", "-rubygems", "-e", "puts Gem.user_dir"]
 
     def __init__(self, *args, **kwargs):
-        Plugin.__init__(self, *args, **kwargs)
-        self.sass_process = None
-        rubyPath = subprocess.check_output(self.rubyPathCmd).decode().strip()
-        self.sassPath = os.path.join(rubyPath, 'bin', 'sass')
+        super().__init__(*args, **kwargs)
+        self.sassProcess = None
+        rubyBin = subprocess.check_output(self.rubyPathCmd).decode().strip()
+        self.sassPath = os.path.join(rubyBin, 'bin', 'sass')
         reporter.report_generic('Sass path: %s' % self.sassPath)
 
-    def is_enabled(self, extra_flags):
-        return bool(extra_flags.get('sass'))
-
-    def run_sass(self, watch=False):
+    def run_sass(self, watch=True):
         args = [self.sassPath, '--no-cache']
         if watch:
             args.append('--watch')
         args.append('assets/static')
-        return portable_popen(args, cwd=self.env.root_path)
+        return subprocess.Popen(args, cwd=self.env.root_path)
+
+    def is_enabled(self, extra_flags):
+        return bool(extra_flags.get('sass'))
 
     def on_server_spawn(self, **extra):
-        extra_flags = (
-            extra.get("extra_flags") or extra.get("build_flags") or {})
-        if not self.is_enabled(extra_flags):
+        flags = extra.get("extra_flags") or extra.get("build_flags") or {}
+        if not self.is_enabled(flags):
             return
 
         reporter.report_generic('Spawning sass watcher')
-        self.sass_process = self.run_sass(watch=True)
+        self.sassProcess = self.run_sass()
 
-    # noinspection PyUnusedLocal
-    def on_server_stop(self, **extra):
-        if self.sass_process is not None:
+    def on_server_stop(self, **_):
+        if self.sassProcess:
             reporter.report_generic('Stopping sass watcher')
-            self.sass_process.kill()
+            self.sassProcess.kill()
 
-    # noinspection PyUnusedLocal
-    def on_before_build_all(self, builder, **extra):
-        extra_flags = getattr(
-            builder, "extra_flags", getattr(builder, "build_flags", None)
-        )
-        if not self.is_enabled(extra_flags) \
-           or self.sass_process is not None:
+    def on_before_build_all(self, builder, **_):
+        flags = getattr(
+            builder, "extra_flags", getattr(builder, "build_flags", None))
+        if not self.is_enabled(flags) or self.sassProcess is not None:
             return
-        self.npm_install()
+
         reporter.report_generic('Starting sass build')
-        self.run_sass().wait()
+        self.run_sass(watch=False).wait()
         reporter.report_generic('Sass build finished')
