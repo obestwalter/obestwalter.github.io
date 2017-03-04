fa0f299997642cd78da602d65d4336a93b19fcd9
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-03-05 00:08:58 +0100

    admonitions (developing awesome sass foo :))

--- packages/lektor-markdown-admonition/lektor_markdown_admonition.py
+++ packages/lektor-markdown-admonition/lektor_markdown_admonition.py
@@ -2,26 +2,24 @@ import re
 
 from lektor.pluginsystem import Plugin
 
-_prefix_re = re.compile(r'^\s*(!{1,4})\s+')
 
-CLASSES = {
-    1: 'note',
-    2: 'info',
-    3: 'tip',
-    4: 'warning',
-}
+class AdmonitionMixin:
+    REGEX = re.compile(r'^\s*(!{1,4})\s+')
+    CLASSES = {
+        1: 'note',
+        2: 'info',
+        3: 'tip',
+        4: 'warning'}
 
-
-class AdmonitionMixin(object):
     def paragraph(self, text):
-        match = _prefix_re.match(text)
+        match = self.REGEX.match(text)
         if match is None:
             # noinspection PyUnresolvedReferences
-            return super(AdmonitionMixin, self).paragraph(text)
+            return super().paragraph(text)
+
         level = len(match.group(1))
-        return '<div class="admonition admonition-%s"><p>%s</p></div>' % (
-            CLASSES[level],
-            text[match.end():])
+        return '<div class="admonition-%s"><p>%s</p></div>' % (
+            self.CLASSES[level], text[match.end():])
 
 
 class MarkdownAdmonitionPlugin(Plugin):
