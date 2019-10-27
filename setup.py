242c545067b39b1f0dd5071953e53ee583ae0dc9
Oliver Bestwalter <oliver.bestwalter@avira.com>
2019-10-27 20:00:38 +0100

    update dependencies

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     url='https://github.com/obestwalter/obestwalter.github.io',
     version='19.11dev1',
     packages=['lebut'],
+    install_requires=["python-slugify", "nbformat", "nbconvert"],
     entry_points={'console_scripts': ['lebut = lebut.cli:main']},
 )
