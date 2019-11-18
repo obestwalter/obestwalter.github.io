bc3d5852c765e3f211f717f20b82914acbab268b
Oliver Bestwalter <oliver.bestwalter@avira.com>
2019-11-18 20:18:47 +0100

    update website article

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,62 +1,62 @@
-import shutil
-from pathlib import Path
-
-import pytest
-
-from lektor.admin.webui import WebUI
-from lektor.builder import Builder
-from lektor.db import Database
-from lektor.environment import Environment
-from lektor.project import Project
-
-# duplicated knowledge -> not worth fixture or shared importable module
-HERE = Path(__file__).parent
-OUTPUT_PATH = HERE / "build"
-PROJECT_PATH = HERE / "project"
-
-
-def make_test_project():
-    project = Project.from_path(PROJECT_PATH)
-    project.get_output_path = lambda *_: OUTPUT_PATH
-    assert project
-    return project
-
-
-@pytest.fixture
-def build_path():
-    path = HERE / "build"
-    shutil.rmtree(path, ignore_errors=True)
-    assert not path.exists()
-    return path
-
-
-@pytest.fixture
-def project():
-    return make_test_project()
-
-
-@pytest.fixture
-def env(project):
-    return Environment(project)
-
-
-@pytest.fixture
-def pad(env):
-    return Database(env).new_pad()
-
-
-@pytest.fixture
-def builder(pad, build_path):
-    yield Builder(pad, build_path)
-
-
-@pytest.fixture
-def F():
-    from lektor.db import F
-
-    return F
-
-
-@pytest.fixture
-def web_ui(tmpdir, env):
-    yield WebUI(env, output_path=str(tmpdir))
+# import shutil
+# from pathlib import Path
+#
+# import pytest
+#
+# from lektor.admin.webui import WebUI
+# from lektor.builder import Builder
+# from lektor.db import Database
+# from lektor.environment import Environment
+# from lektor.project import Project
+#
+# # duplicated knowledge -> not worth fixture or shared importable module
+# HERE = Path(__file__).parent
+# OUTPUT_PATH = HERE / "build"
+# PROJECT_PATH = HERE / "project"
+#
+#
+# def make_test_project():
+#     project = Project.from_path(PROJECT_PATH)
+#     project.get_output_path = lambda *_: OUTPUT_PATH
+#     assert project
+#     return project
+#
+#
+# @pytest.fixture
+# def build_path():
+#     path = HERE / "build"
+#     shutil.rmtree(path, ignore_errors=True)
+#     assert not path.exists()
+#     return path
+#
+#
+# @pytest.fixture
+# def project():
+#     return make_test_project()
+#
+#
+# @pytest.fixture
+# def env(project):
+#     return Environment(project)
+#
+#
+# @pytest.fixture
+# def pad(env):
+#     return Database(env).new_pad()
+#
+#
+# @pytest.fixture
+# def builder(pad, build_path):
+#     yield Builder(pad, build_path)
+#
+#
+# @pytest.fixture
+# def F():
+#     from lektor.db import F
+#
+#     return F
+#
+#
+# @pytest.fixture
+# def web_ui(tmpdir, env):
+#     yield WebUI(env, output_path=str(tmpdir))
--- tests/test_lebut.py
+++ tests/test_lebut.py
@@ -1,78 +1,74 @@
 """Primitive high level tests: if nothing crashes things look good."""
+import shutil
 from pathlib import Path
 
 import pytest
 
 import lektor.pluginsystem
-from lebut.config import PATH
 from lebut.lektor_monkeypatch import initialize_plugins_with_local_plugins
-from lebut.plugin_ipynb_to_md import source_obj_to_ipynb
 from lektor.builder import Builder
-from lektor.cli import build_cmd, clean_cmd
 from lektor.db import Query, Page
+from lektor.project import Project
 from lektor.reporter import CliReporter
 
-# duplicated knowledge -> not worth fixture or shared importable module
 HERE = Path(__file__).parent
 OUTPUT_PATH = HERE / "build"
-PROJECT_PATH = HERE / "project"
+PROJECT_PATH = HERE.parent
+
+
+@pytest.fixture(scope="session", autouse=True)
+def prepare_for_tests():
+    # no tidying up necessary- will never use original
+    lektor.pluginsystem.initialize_plugins = initialize_plugins_with_local_plugins
+    shutil.rmtree(OUTPUT_PATH, ignore_errors=True)
 
 
 @pytest.fixture
-def patched_plugins(monkeypatch):
-    monkeypatch.setattr(
-        lektor.pluginsystem, "initialize_plugins", initialize_plugins_with_local_plugins
-    )
+def project():
+    project = Project.from_path(PROJECT_PATH)
+    project.get_output_path = lambda *_: OUTPUT_PATH
+    assert project
+    return project
 
 
-@pytest.mark.xfail(
-    reason="jinja2.exceptions.UndefinedError: 'get_pygments_stylesheet' is undefined"
-)
 @pytest.mark.parametrize(
     "slug, root",
     (
-        # ("about", "/"),
-        ("notebook-post", "blog"),
+        ("/", "/"),
+        ("website-meta", "articles"),
     ),
 )
-@pytest.mark.usefixtures("patched_plugins")
-def test_single_page_build(env, slug, root):
+def test_build(project, slug, root):
+    # for partial tests and debugging
+    env = project.make_env()
     reporter = CliReporter(env, verbosity=4)
     with reporter:
-        env.load_plugins()
         pad = env.new_pad()
         builder = Builder(pad, OUTPUT_PATH)
         query = Query(root, pad)
         page: Page = query.get(slug)
         assert page
-        for attachment in page.attachments:
-            source_obj_to_ipynb(attachment)
-        # FIXME make fail and make sure error bubbles up
         builder.build(page)
-        # FIXME generating actual html does not yield anything this way
-        # out = Path(project.get_output_path()) / page.path[1:] / "index.html"
-        # text = out.read_text()
-        # assert len(text) > 100
+        path = OUTPUT_PATH / page.path[1:] / "index.html"
+        text = path.read_text()
+        assert len(text) > 100  # yes, I know ...
 
 
-@pytest.mark.xfail(
-    reason="jinja2.exceptions.UndefinedError: 'get_pygments_stylesheet' is undefined"
-)
-@pytest.mark.usefixtures("patched_plugins")
-def test_complete_build_test_project(monkeypatch):
-    monkeypatch.chdir(PROJECT_PATH)
-    build_cmd(["-vvvv", "--output-path", str(OUTPUT_PATH)])
-
-
-@pytest.mark.usefixtures("patched_plugins")
-def test_complete_build_my_site(monkeypatch):
-    monkeypatch.chdir(PATH.PROJECT)
-    try:
-        clean_cmd(["--yes", "-vvvv", "--output-path", str(PATH.OUTPUT)])
-    except SystemExit as e:
-        assert e.code == 0
-
-    try:
-        build_cmd(["-vvvv", "--output-path", str(OUTPUT_PATH)])
-    except SystemExit as e:
-        assert e.code == 0
+# testing through click is pretty awful on first sight... look into this later
+#
+# def test_complete_build_test_project(monkeypatch):
+#     monkeypatch.chdir(PROJECT_PATH)
+#     build_cmd(["-vvvv", "--output-path", str(OUTPUT_PATH)])
+#
+#
+# def test_complete_build_my_site(monkeypatch):
+#     monkeypatch.chdir(PATH.PROJECT)
+#     try:
+#         clean_cmd(["--yes", "-vvvv", "--output-path", str(PATH.OUTPUT)])
+#     except SystemExit as e:
+#         assert e.code == 0
+#
+#     try:
+#         build_cmd(["-vvvv", "--output-path", str(OUTPUT_PATH)])
+#     except SystemExit as e:
+#         assert e.code == 0
